ba7940bfb079f6da70545b5e246d4fe8fee500d6
Yashodhar Patel <patelyashodhar012@gmail.com>
2021-10-10 16:15:15 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/src', express.static('src'));
+app.use('/static', express.static('static'));
+app.use('/views', express.static('views'));
+app.use(express.urlencoded());
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+// START THE SERVER
+app.listen(port, () => {
+	console.log(`The Application is running successfully on port ${port}`);
+});
